554df31e3c0f4ba7015111a64200ae27734d4dbf
tomit4 <mosssap@gmail.com>
2024-06-02 16:42:22 -0700

    :rotating_light: Fixed pyright linter warnings

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,3 +5,5 @@ class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     user_name = models.CharField(max_length=30)
     user_email = models.EmailField(max_length=254)
+
+    objects = models.Manager()
