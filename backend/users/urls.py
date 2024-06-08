432b5cbed9c813d1cdf80c80b350b58da85d0b89
tomit4 <mosssap@gmail.com>
2024-06-07 22:18:27 -0700

    :recycle: Refactored login logic, added path

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,5 +7,7 @@ app_name = 'users'
 urlpatterns = [
     re_path('api/register-by-access-token/' + r'social/(?P<backend>[^/]+)/$',
             views.register_by_access_token),
+    re_path('api/login-by-access-token/' + r'social/(?P<backend>[^/]+)/$',
+            views.login_by_access_token),
     path('api/authentication-test/', views.authentication_test),
 ]
