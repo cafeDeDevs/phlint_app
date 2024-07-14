79ce41c5bc7753e6990e43477e5b548dfdfb06b4
tomit4 <mosssap@gmail.com>
2024-07-13 19:31:57 -0700

    :recycle: Refactor for more direct use of routes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, re_path
 
-from . import views
+from users.routes.auth import register_by_access_token, login_by_access_token, logout_view, authentication_test
+from users.routes.gallery import get_gallery_test
 
 app_name = 'users'
 
 urlpatterns = [
     re_path('api/register-by-access-token/' + r'social/(?P<backend>[^/]+)/$',
-            views.register_by_access_token),
+            register_by_access_token),
     re_path('api/login-by-access-token/' + r'social/(?P<backend>[^/]+)/$',
-            views.login_by_access_token),
-    re_path('api/logout/' + r'social/(?P<backend>[^/]+)/$', views.logout_view),
-    path('api/authentication-test/', views.authentication_test),
-    path('api/get-gallery-test/', views.get_gallery_test),
+            login_by_access_token),
+    re_path('api/logout/' + r'social/(?P<backend>[^/]+)/$', logout_view),
+    path('api/authentication-test/', authentication_test),
+    path('api/get-gallery-test/', get_gallery_test),
 ]
--- backend/users/views.py
+++
@@ -1,2 +0,0 @@
-from users.routes.auth import logout_view, register_by_access_token, login_by_access_token, authentication_test
-from users.routes.gallery import get_gallery_test
