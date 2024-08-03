04fb937676aec176e2642b9baeb3a1a6a75ef9fe
tomit4 <mosssap@gmail.com>
2024-08-03 13:38:12 -0700

    :recycle: Organized zod auth logic into utils file

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 const grabStoredCookie = (cookieKey: string): string => {
     const cookies: { [key: string]: string } = document.cookie
         .split('; ')
@@ -14,4 +15,27 @@ const delay = (ms: number): Promise<void> => {
     return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-export { grabStoredCookie, delay }
+const passwordSchemaRegex = new RegExp(
+    [
+        /^(?=.*[a-z])/, // At least one lowercase letter
+        /(?=.*[A-Z])/, // At least one uppercase letter
+        /(?=.*\d)/, // At least one digit
+        /(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])/, // At least one special character
+        /[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{10,}$/, // At least 10 characters long
+    ]
+        .map((r) => r.source)
+        .join(''),
+)
+
+const passwordSchema = z
+    .string()
+    .regex(
+        passwordSchemaRegex,
+        'Password must be at least 10 characters long and contain at least one lowercase letter, one uppercase letter, one digit, and one special character.',
+    )
+
+const usernameSchema = z
+    .string()
+    .min(5, 'Username must be at least 5 characters long.')
+
+export { grabStoredCookie, delay, passwordSchema, usernameSchema }
