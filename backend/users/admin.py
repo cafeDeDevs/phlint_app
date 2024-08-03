8889294723967d0841be9069fbfea4d58799d073
tomit4 <mosssap@gmail.com>
2024-08-03 13:36:24 -0700

    :recycle: Removed unnecessary adjustments, users are not admins

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
 
-class UserAdmin(BaseUserAdmin):
-    ordering = ('email',)
-    list_display = ('email', 'user_name', 'is_staff', 'is_active')
-    fieldsets = (
-        (None, {'fields': ('email', 'user_name', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-        ('Important dates', {'fields': ('last_login',)}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'user_name', 'password1', 'password2', 'is_staff', 'is_active'),
-        }),
-    )
-
-admin.site.register(User, UserAdmin)
+# Register your models here.
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,43 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, user_name, password=None, **extra_fields):
 
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, user_name, password=None, **extra_fields):
-
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, user_name, password, **extra_fields)
-
-class User(AbstractBaseUser, PermissionsMixin):
+class User(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     user_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254, unique=True)
-    user_password = models.CharField(max_length=512, null=True, blank=True) 
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name']
-
-    def __str__(self):
-        return self.email
+    user_password = models.CharField(max_length=254, null=True)
+    user_email = models.EmailField(max_length=254, null=True)
+    is_active = models.BooleanField(False, null=True)
+    objects = models.Manager()
 
 
 class Albums(models.Model):
