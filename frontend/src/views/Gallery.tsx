f50f84717fed4cf488ca64d46b20054ad322b905
tomit4 <mosssap@gmail.com>
2024-08-04 07:16:29 -0700

    :wrench: Added credentials to be included in call to backend

--- frontend/src/views/Gallery.tsx
+++ frontend/src/views/Gallery.tsx
@@ -18,6 +18,7 @@ const Gallery = () => {
                     import.meta.env.VITE_BACKEND_GALLERY_ROUTE,
                     {
                         method: 'GET',
+                        credentials: 'include',
                     },
                 )
                 if (!galleryRes.ok)
