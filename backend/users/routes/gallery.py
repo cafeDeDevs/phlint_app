c34321f414899883f4a24549aff0edf59077296e
tomit4 <mosssap@gmail.com>
2024-08-04 07:14:38 -0700

    :construction: Set up very rough implementation of private galleries

--- backend/users/routes/gallery.py
+++ backend/users/routes/gallery.py
@@ -12,23 +12,36 @@ from users.utils.s3_utils import *
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def get_gallery_test(request) -> Response:
+def get_gallery_test(request, backend) -> Response:
+    # TODO: grab user_name using request.user to query db
+    # and use that instead of the email to name default s3 directory,
+    # we'll need Google OAuth Signup/Onboarding to do this
     s3_client = boto3.client('s3')
     try:
         file_list = grab_file_list()
         image_files = []
 
-        if len(file_list) != 0:
-            for file in file_list:
-                if '/' in file:
-                    file_obj = s3_client.get_object(
-                        Bucket='phlint-app-s3-test', Key=file)
-                    image_data = base64.b64encode(
-                        file_obj['Body'].read()).decode('utf-8')
-                    image_files.append(image_data)
-        else:
-            return Response({'message': 'No Images In Your Gallery Found.'},
-                            status=status.HTTP_404_NOT_FOUND)
+        # TODO: Don't rely on this, refactor to create new buckets instead of directories
+        if len(file_list) == 0:
+            upload_file('./users/assets/default.jpg', 'phlint-app-s3-test',
+                        'default.jpg')
+
+        for file in file_list:
+            # NOTE: Creates a default album/file if first time seeing gallery
+            if f'{request.user}/default/' not in file:
+                upload_file('./users/assets/default.jpg', 'phlint-app-s3-test',
+                            f'{request.user}/default/default.jpg')
+        for file in file_list:
+            if f'{request.user}/default/' in file:
+                file_obj = s3_client.get_object(Bucket='phlint-app-s3-test',
+                                                Key=file)
+                image_data = base64.b64encode(
+                    file_obj['Body'].read()).decode('utf-8')
+                image_files.append(image_data)
+
+        # Use this once buckets are used
+        #  return Response({'message': 'No Images In Your Gallery Found.'},
+        #  status=status.HTTP_404_NOT_FOUND)
         return Response(
             {
                 'message':
--- backend/users/utils/s3_utils.py
+++ backend/users/utils/s3_utils.py
@@ -1,9 +1,15 @@
 import logging
 import boto3
+import os
 
 logger = logging.getLogger(__name__)
 
 
+# NOTE: Potentially Very expensive...
+# queries entire bucket every time any user visits to gallery
+# TODO: change so that for loop checks for user_name and
+# album_name before populating file_list
+# NOTE: Maybe creating a new bucket per user isn't such a bad idea after all...
 def grab_file_list():
     try:
         file_list = []
@@ -16,3 +22,16 @@ def grab_file_list():
     except Exception as e:
         logger.error("S3 error: %s", str(e))
         return []
+
+
+def upload_file(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+    except Exception as e:
+        logging.error(e)
+        return False
+    return True
