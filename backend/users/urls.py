f97d43498aaa51d9ccde9472bb3810821bd50970
Paul <paulcapob@gmail.com>
2024-08-08 18:01:27 +0900

    Added url imports

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.routes.auth import *
 from users.routes.gallery import *
 
@@ -17,4 +17,6 @@ urlpatterns = [
             get_gallery_test),
     path('api/email-registration/', email_registration_view),
     path('api/activate/', activate, name='activate'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
