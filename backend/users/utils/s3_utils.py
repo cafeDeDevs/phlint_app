f25021c5a97907222fb1174d828e302df9d6d003
tomit4 <mosssap@gmail.com>
2024-08-10 23:57:53 -0700

    :recycle: Refactored upload_image logic to direct stream image

--- backend/users/utils/s3_utils.py
+++ backend/users/utils/s3_utils.py
@@ -47,13 +47,20 @@ def grab_file_list(bucket) -> List[str]:
         return []
 
 
-def upload_file(file_name, bucket, object_name=None) -> bool:
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
+def upload_file(file, bucket, object_name=None, gallery_name=None) -> bool:
     s3_client = boto3.client('s3')
+    if gallery_name is None:
+        gallery_name = 'default/'
     try:
-        s3_client.upload_file(file_name, bucket, object_name)
+        # NOTE: If object_name is not NONE, then get file
+        # from file-like object (direct upload)
+        if object_name is not None:
+            object_name = os.path.join(gallery_name, os.path.basename(file))
+            s3_client.upload_file(file, bucket, object_name)
+        # NOTE: Otherwise, get file from file system (hard drive)
+        else:
+            object_name = os.path.join(gallery_name, file.name)
+            s3_client.upload_fileobj(file, bucket, object_name)
     except Exception as e:
         logging.error(e)
         return False
